89d0f00598ea58b2e3b92a41a7d0b5b79b2cb626
John Carlo Miel <johncarlomiel@gmail.com>
2020-07-26 22:48:40 +0800

    :sparkles: P2P Connection (#3)

--- src/emitters/queue.js
+++ src/emitters/queue.js
@@ -7,6 +7,40 @@ let onQueue = [];
 
 const queueChecker = new events.EventEmitter();
 
+queueChecker.on('ready-handshake', ({ roomId }, userId) => {
+  console.log('ready-handshake', userId);
+  console.log(rooms);
+  rooms = rooms.map(room => {
+    const { initiator, receiver, roomId: storedRoomId } = room;
+    const clients = [initiator, receiver];
+    if (storedRoomId === roomId) {
+      if (initiator.userId === userId) {
+        initiator.readyToHandshake = true;
+      }
+
+      if (receiver.userId === userId) {
+        receiver.readyToHandshake = true;
+      }
+
+      handshakeReady = clients.every(({ readyToHandshake }) => readyToHandshake);
+
+      if (handshakeReady) {
+        const createHandshakePayload = (role, userId) => {
+          return {
+            data: { roomId, role, username: userId },
+            type: 'handshake-ready'
+          };
+        };
+        sendMessage(initiator.clientWs, createHandshakePayload('initiator', initiator.userId));
+        sendMessage(receiver.clientWs, createHandshakePayload('receiver', receiver.userId));
+      }
+
+      console.log('Handshake Ready', handshakeReady);
+    }
+    return room;
+  });
+});
+
 queueChecker.on('new-client-queuing', (client, userId) => {
   onQueue.push({ userId, client });
   if (onQueue.length >= 2) {
@@ -27,7 +61,8 @@ queueChecker.on('new-client-queuing', (client, userId) => {
         newRoom[role] = {
           clientWs: client,
           userId,
-          status: 'pending'
+          status: 'pending',
+          readyToHandshake: false
         }
         return sendMessage(
           client,
@@ -44,7 +79,6 @@ queueChecker.on('new-client-queuing', (client, userId) => {
     rooms.push(newRoom);
   }
 });
-
 
 queueChecker.on('cancel-queue', (userId) => {
   onQueue = onQueue.filter(({ userId: clientUserId }) => userId !== clientUserId)
@@ -82,19 +116,72 @@ queueChecker.on('respond-to-invitation', ({ roomId, response }, userId) => {
 
 
   if (response === 'reject') {
-    const test = rooms.find(({ roomId: storedRoomId }) => storedRoomId === roomId);
     const { initiator, receiver } = rooms.find(({ roomId: storedRoomId }) => storedRoomId === roomId);
     const clients = [initiator, receiver];
 
-    clients.forEach(({ clientWs, userId:storedUserId }) => {
+    clients.forEach(({ clientWs, userId: storedUserId }) => {
       if (storedUserId !== userId) {
         sendMessage(clientWs, { type: 'break-match' })
       }
-    }); 
+    });
     rooms = rooms.filter(({ roomId: storedRoomId }) => storedRoomId !== roomId);
   }
 
 });
 
+queueChecker.on('add-candidate', ({ roomId, role, candidate }, userId) => {
+  console.log('Add Candidate event: ');
+  console.log({ roomId, role, candidate });
+  const oppositeRole = getOppositeRole(role);
+  const room = rooms.find(room => room.roomId === roomId);
+  const { clientWs } = room[oppositeRole];
+
+  sendMessage(clientWs, {
+    type: 'add-candidate',
+    data: {
+      candidate
+    }
+  });
+
+});
+
+queueChecker.on('receive-offer', ({ roomId, offer }) => {
+  console.log('Receive offer event: ');
+  console.log({ roomId, offer });
+  // When the initiator send the offer to the server
+  // the server will send it to the receiver
+  const room = rooms.find(room => room.roomId === roomId);
+  const { clientWs } = room['receiver'];
+
+  sendMessage(clientWs, {
+    type: 'offer',
+    data: {
+      offer
+    }
+  });
+});
+
+queueChecker.on('receive-answer', ({ roomId, answer }) => {
+
+  console.log('Receive answer event: ');
+  console.log({ roomId, answer });
+  // When the receiver send the answer to the offer
+  // send to initiator the offer
+  const room = rooms.find(room => room.roomId === roomId);
+  console.log('Error', room);
+  const { clientWs } = room['initiator'];
+
+  sendMessage(clientWs, {
+    type: 'answer',
+    data: {
+      answer
+    }
+  });
+});
+
+
+function getOppositeRole(role) {
+  return role === 'initiator' ? 'receiver' : 'initiator';
+}
 
 module.exports = queueChecker;
--- src/models/WebSocketServer.js
+++ src/models/WebSocketServer.js
@@ -22,5 +22,4 @@ class WebSocketServer {
     }
 }
 
-
 module.exports = WebSocketServer;
