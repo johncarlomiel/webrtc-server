0c67063bb45f8241b1e4416f5a44283190729cae
John Carlo Miel <johncarlomiel@gmail.com>
2020-08-22 09:26:12 +0800

    :sparkles: Release v1.0 (#4)
    Co-authored-by: John Carlo Miel <john.miel@tooltwist.com>

--- src/emitters/queue.js
+++ src/emitters/queue.js
@@ -1,28 +1,24 @@
 const events = require('events');
 const { v1: uuidv1, v4: uuidv4 } = require('uuid');
 const { sendMessage } = require('../utils/index');
-
-let rooms = [];
-let onQueue = [];
+const store = require('../configs/store');
+const { onQueue } = require('../configs/store');
 
 const queueChecker = new events.EventEmitter();
 
 queueChecker.on('ready-handshake', ({ roomId }, userId) => {
-  console.log('ready-handshake', userId);
-  console.log(rooms);
-  rooms = rooms.map(room => {
-    const { initiator, receiver, roomId: storedRoomId } = room;
-    const clients = [initiator, receiver];
+  store.rooms = store.rooms.map(room => {
+    const { participants, roomId: storedRoomId } = room;
     if (storedRoomId === roomId) {
-      if (initiator.userId === userId) {
-        initiator.readyToHandshake = true;
-      }
+      room.participants = room.participants.map((participant) => {
+        if (participant.userId === userId) {
+          participant.readyToHandshake = true;
+        }
 
-      if (receiver.userId === userId) {
-        receiver.readyToHandshake = true;
-      }
+        return participant
+      });
 
-      handshakeReady = clients.every(({ readyToHandshake }) => readyToHandshake);
+      handshakeReady = participants.every(({ readyToHandshake }) => readyToHandshake);
 
       if (handshakeReady) {
         const createHandshakePayload = (role, userId) => {
@@ -31,22 +27,28 @@ queueChecker.on('ready-handshake', ({ roomId }, userId) => {
             type: 'handshake-ready'
           };
         };
-        sendMessage(initiator.clientWs, createHandshakePayload('initiator', initiator.userId));
-        sendMessage(receiver.clientWs, createHandshakePayload('receiver', receiver.userId));
-      }
 
-      console.log('Handshake Ready', handshakeReady);
+        const { userId: initiatorUserId } = participants.find(({ role }) => role === 'initiator');
+        const { ws: initiatorWs } = store.findClient(initiatorUserId);
+
+        const { userId: receiverUserId } = participants.find(({ role }) => role === 'receiver');
+        const { ws: receiverWs } = store.findClient(receiverUserId);
+
+        sendMessage(initiatorWs, createHandshakePayload('initiator', initiatorUserId));
+        sendMessage(receiverWs, createHandshakePayload('receiver', receiverUserId));
+      }
     }
     return room;
   });
 });
 
 queueChecker.on('new-client-queuing', (client, userId) => {
-  onQueue.push({ userId, client });
-  if (onQueue.length >= 2) {
+  store.onQueue = store.onQueue.concat(userId);
+  if (store.onQueue.length >= 2) {
     const roomId = uuidv4();
     let newRoom = {
-      roomId
+      roomId,
+      participants: []
     };
 
     let payload = {
@@ -55,17 +57,20 @@ queueChecker.on('new-client-queuing', (client, userId) => {
 
     const roles = ['initiator', 'receiver'];
 
-    onQueue = onQueue.filter(({ client, userId }, clientIndex) => {
+    store.onQueue = store.onQueue.filter((userId, clientIndex) => {
       if (clientIndex !== 2) {
         const role = roles[clientIndex];
-        newRoom[role] = {
-          clientWs: client,
+        const { ws } = store.findClient(userId);
+
+        newRoom.participants.push({
           userId,
+          role,
           status: 'pending',
           readyToHandshake: false
-        }
+        });
+
         return sendMessage(
-          client,
+          ws,
           Object.assign(payload, {
             data: {
               role, roomId
@@ -76,67 +81,71 @@ queueChecker.on('new-client-queuing', (client, userId) => {
 
       return false;
     });
-    rooms.push(newRoom);
+    store.rooms = store.rooms.concat(newRoom);
   }
 });
 
-queueChecker.on('cancel-queue', (userId) => {
-  onQueue = onQueue.filter(({ userId: clientUserId }) => userId !== clientUserId)
+queueChecker.on('cancel-queue', (clientUserId) => {
+  store.onQueue = store.onQueue.filter((userId) => userId !== clientUserId)
 });
 
 queueChecker.on('respond-to-invitation', ({ roomId, response }, userId) => {
   if (response === 'accept') {
-    rooms = rooms.map((room) => {
-      const { initiator, receiver, roomId: storedRoomId } = room;
-      const clients = [initiator, receiver];
+    store.rooms = store.rooms.map((room) => {
+      const { participants, roomId: storedRoomId } = room;
       if (storedRoomId === roomId) {
-        if (initiator.userId === userId) {
-          initiator.status = 'accepted';
-        }
+        room.participants = room.participants.map((participant) => {
+          if (participant.userId === userId) {
+            participant.status = 'accepted';
+          }
 
-        if (receiver.userId === userId) {
-          receiver.status = 'accepted';
-        }
+          return participant;
+        });
 
-        matchReady = clients.every(({ status }) => status === 'accepted');
+        matchReady = participants.every(({ status }) => status === 'accepted');
 
         if (matchReady) {
           const payload = {
             data: { roomId },
             type: 'match-ready'
           };
-          sendMessage(initiator.clientWs, payload);
-          sendMessage(receiver.clientWs, payload);
+
+          const { userId: initiatorUserId } = participants.find(({ role }) => role === 'initiator');
+          const { ws: initiatorWs } = store.findClient(initiatorUserId);
+
+          const { userId: receiverUserId } = participants.find(({ role }) => role === 'receiver');
+          const { ws: receiverWs } = store.findClient(receiverUserId);
+
+          sendMessage(initiatorWs, payload);
+          sendMessage(receiverWs, payload);
         }
       }
-
       return room;
     });
   }
 
-
   if (response === 'reject') {
-    const { initiator, receiver } = rooms.find(({ roomId: storedRoomId }) => storedRoomId === roomId);
-    const clients = [initiator, receiver];
+    const { participants } = store.rooms.find(({ roomId: storedRoomId }) => storedRoomId === roomId);
 
-    clients.forEach(({ clientWs, userId: storedUserId }) => {
+    participants.forEach(({ userId: storedUserId }) => {
       if (storedUserId !== userId) {
-        sendMessage(clientWs, { type: 'break-match' })
+        const { ws } = store.findClient(storedUserId);
+        sendMessage(ws, { type: 'break-match' })
       }
     });
-    rooms = rooms.filter(({ roomId: storedRoomId }) => storedRoomId !== roomId);
+
+    store.rooms = store.rooms.filter(({ roomId: storedRoomId }) => storedRoomId !== roomId);
   }
 
 });
 
-queueChecker.on('add-candidate', ({ roomId, role, candidate }, userId) => {
-  console.log('Add Candidate event: ');
-  console.log({ roomId, role, candidate });
+queueChecker.on('add-candidate', ({ roomId, role, candidate }) => {
   const oppositeRole = getOppositeRole(role);
-  const room = rooms.find(room => room.roomId === roomId);
-  const { clientWs } = room[oppositeRole];
+  const room = store.rooms.find(room => room.roomId === roomId);
+  const { userId } = room.participants.find((participant) => participant.role === oppositeRole);
+  const { ws } = store.findClient(userId);
 
-  sendMessage(clientWs, {
+  sendMessage(ws, {
     type: 'add-candidate',
     data: {
       candidate
@@ -146,14 +155,13 @@ queueChecker.on('add-candidate', ({ roomId, role, candidate }, userId) => {
 });
 
 queueChecker.on('receive-offer', ({ roomId, offer }) => {
-  console.log('Receive offer event: ');
-  console.log({ roomId, offer });
   // When the initiator send the offer to the server
   // the server will send it to the receiver
-  const room = rooms.find(room => room.roomId === roomId);
-  const { clientWs } = room['receiver'];
+  const room = store.rooms.find(room => room.roomId === roomId);
+  const { userId } = room.participants.find((participant) => participant.role === 'receiver');
+  const { ws } = store.findClient(userId);
 
-  sendMessage(clientWs, {
+  sendMessage(ws, {
     type: 'offer',
     data: {
       offer
@@ -162,20 +170,43 @@ queueChecker.on('receive-offer', ({ roomId, offer }) => {
 });
 
 queueChecker.on('receive-answer', ({ roomId, answer }) => {
-
-  console.log('Receive answer event: ');
-  console.log({ roomId, answer });
   // When the receiver send the answer to the offer
   // send to initiator the offer
-  const room = rooms.find(room => room.roomId === roomId);
-  console.log('Error', room);
-  const { clientWs } = room['initiator'];
+  const room = store.rooms.find(room => room.roomId === roomId);
+  const { userId } = room.participants.find((participant) => participant.role === 'initiator');
+  const { ws } = store.findClient(userId);
 
-  sendMessage(clientWs, {
+  sendMessage(ws, {
     type: 'answer',
     data: {
       answer
     }
+  });
+});
+
+queueChecker.on('disconnect', (userId) => {
+  // Clean the queue 
+  store.onQueue = store.onQueue.filter((storedUserId) => storedUserId !== userId);
+  store.rooms = store.rooms.filter((room) => {
+    // Find all store.rooms that the 
+    // participant in and remove it
+
+    const { participants } = room;
+    const isParticipant = participants.find((participant) => participant.userId === userId);
+    // if user is participant of the room
+    // notify all participant of the disconnection
+    if (isParticipant) {
+      participants.forEach(({ userId }) => {
+        const { ws } = store.findClient(userId);
+        sendMessage(ws, {
+          type: 'disconnect'
+        });
+      });
+    }
+
+    // Return all rooms that the disconnected 
+    // user is not a participant
+    return !isParticipant;
   });
 });
 
--- src/models/WebSocketServer.js
+++ src/models/WebSocketServer.js
@@ -1,22 +1,26 @@
 const webSocket = require('ws');
 const PeerToPeer = require('./PeerToPeer');
+const store = require('../configs/store');
 
 class WebSocketServer {
     constructor(httpServer) {
-        this.wss = new webSocket.Server({server:httpServer});
-        this.clients = [];
+        this.wss = new webSocket.Server({ server: httpServer });
         this.features = {
             peerToPeer: new PeerToPeer()
         }
     }
 
-    initialize(){
+    initialize() {
         this.wss.on('connection', (ws, req) => {
-            var id = req.headers['sec-websocket-key'];
-            this.clients[id] = ws;
+            const userId = req.headers['sec-websocket-key'];
+            store.addClient({ userId, ws });
             ws.on('message', (request) => {
                 const parsedRequest = JSON.parse(request);
-                this.features[parsedRequest.feature].onMessage(parsedRequest, this.clients[id], id);
+                this.features[parsedRequest.feature].onMessage(parsedRequest, ws, userId);
+            });
+
+            ws.on('close', () => {
+                this.features.peerToPeer.onDisconnect(userId);
             });
         });
     }
